0a005d8745144b49daf5572461c122030cd0eedf
Pasindi Vindula <123557234+pasi2001@users.noreply.github.com>
2023-03-07 17:43:21 +0530

    my home

--- src/navbar/index.js
+++ src/navbar/index.js
@@ -8,7 +8,7 @@ function Navbar() {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#h">
-                Home
+                My Home
               </a>
             </li>
             <li className="nav-item">
